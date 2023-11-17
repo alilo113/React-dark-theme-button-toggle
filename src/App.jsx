e67cc2db34a0b6e0ece91f43b1a8e0a8a1ac6830
unknown <aliopdandan@gmail.com>
2023-11-17 18:13:09 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
+
 function App() {
+  const [color, setColor] = useState("white");
+
+  function toggleBtn() {
+    setColor(prevColor => (prevColor === "white" ? "black" : "white"));
+  }
+  
   return (
-    <body style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+    <body style={{
+       display: 'flex',
+       justifyContent: 'center',
+       alignItems: 'center',
+       height: '100vh',
+       backgroundColor: color
+       }}>
       <div>
-        <button className="border border-solid border-black p-3 bg-sky-800 text-white hover:bg-sky-500">
+        <button 
+           className="border border-solid border-black p-3 bg-sky-800 text-white hover:bg-sky-500"
+           onClick={toggleBtn}
+        >
           Toggle
         </button>
       </div>
