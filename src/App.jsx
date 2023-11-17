c14dc2d5b6213f5376c80a1345ebe9c49700139c
unknown <aliopdandan@gmail.com>
2023-11-17 17:36:02 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <body style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <div>
+        <button className="border border-solid border-black p-3 bg-sky-800 text-white hover:bg-sky-500">
+          Toggle
+        </button>
+      </div>
+    </body>
+  )
+}
+
+export default App
